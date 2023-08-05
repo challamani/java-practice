3f23b91686f5ca9f1645a963bd9d1e3035265827
Mani Challa <mani.challa1990@gmail.com>
2023-08-05 20:49:37 +0100

    Pascal triangle approach2

--- src/com/practice/arrays/PascalTriangle.java
+++ src/com/practice/arrays/PascalTriangle.java
@@ -32,30 +32,44 @@ public class PascalTriangle implements MyTechHub {
         return n * factorial(n-1);
     }
 
+    //Print pascal triangle using arrays
     public void approach2(int range){
-        //using arrays
+        //declare an array
         int[][] ncr = new int[range][];
-        //declare size for each row
-        for(int i=1;i<=range; i++){
-            ncr[i-1] = new int[i];
+        //init array size for each row
+        for(int i=0; i<range; i++){
+            ncr[i] = new int[i+1];
         }
 
-        for(int n=0; n<range; n++) {
-            for (int r = 0; r <= n; r++) {
-                if (r == 0 || r == n) {
+        //calculate the pascal triangle values
+        for(int n=0; n<range; n++){
+            for(int r=0; r<=n; r++){
+                if(r ==0 || r == n){
                     ncr[n][r] = 1;
-                } else {
-                    ncr[n][r] = ncr[n - 1][r - 1] + ncr[n - 1][r];
+                }else{
+                    ncr[n][r] = (ncr[n-1][r-1]+ncr[n-1][r]);
                 }
             }
         }
-        
+
+        //print - pascal triangle values
+        for(int n=0; n<range; n++){
+            //display alignment
+            for(int x=0; x<range-n; x++){
+                System.out.print(" ");
+            }
+            for (int r=0; r<=n; r++){
+                System.out.print(" "+ncr[n][r]);
+            }
+            System.out.println();
+        }
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int range = scanner.nextInt();
         new PascalTriangle()
-                .printPascalTriangle(range);
+                .approach2(range);
     }
 }
